ba21335963cde0457966625ae0e993021d083f02
nobcoder <m.argareksapati21@gmail.com>
2026-07-16 22:03:17 +0700

    hari pertama
    Added comments explaining variable usage and definitions.

--- variabel.py
+++ variabel.py
@@ -1,3 +1,8 @@
+#variabel adalah tempat atau wadah untuk menyimpan data atau nilai
+#disini presiden dan bahlil adalah variabel
+#sedangkan Jokowi dan Ganteng adalah String
+# fungsi sama dengan disini (=) adalah sebagai operator yang bekerja
+
 presiden = "Jokowi"
 bahlil = "Ganteng"
 
