6e6ce3c5fa71d622f8fb515e03e0b9c3ecd0f566
nobcoder <m.argareksapati21@gmail.com>
2026-07-16 22:16:27 +0700

    Add files via upload
    hari pertama

---
+++ contohperkalian.py
@@ -0,0 +1,8 @@
+#contoh pertama
+score1 = 5
+score2 = 100
+print(score1 * score2)
+
+#contoh kedua
+score = 789 * 678
+print(score)
---
+++ contohpertambahan.py
@@ -0,0 +1,6 @@
+point1 = 10
+point2 = 258
+print(point1 + point2)
+
+point3 = 538 + 1230
+print(point3)
