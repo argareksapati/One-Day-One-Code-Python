7ce06dee072ff680484d850016e2e3c0277424d4
nobcoder <m.argareksapati21@gmail.com>
2026-07-17 23:20:46 +0700

    hari kedua

---
+++ tipedata.py
@@ -0,0 +1,16 @@
+#String
+nama = "Jokowi"
+
+#Integer
+umur = 50
+
+#float
+suhu = 10.7
+print (nama, umur, suhu)
+
+kota = "Bandung" #string
+umur = 190 #integer
+penduduk = 25.200 #float
+print(type(kota)) #output <class 'str'>
+print(type(umur)) #output <class 'int'>
+print(type(penduduk)) #output <class 'float'>
