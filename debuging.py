fe70cb72a78d38b4d4fa9e41ecda616202dbfd6d
nobcoder <m.argareksapati21@gmail.com>
2026-07-17 22:19:41 +0700

    hari ke dua

---
+++ debuging.py
@@ -0,0 +1,13 @@
+#debuging adalah memperbaiki kode yang error dan tidak sinkron
+
+#contoh
+nama = "Jokowi"
+umur = 67
+gaji -> 50.0000
+
+#disini gaji error dan code akan berhenti di barivs ke dua yaitu umur karna pada gaji menggunakan ->
+
+#contoh yang benar
+nama = "Jokowi"
+umur = 67
+gaji = 50.000
