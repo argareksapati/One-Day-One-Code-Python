a5e06d714c8059d7aebfd279d9dd277d2fd18e7d
nobcoder <m.argareksapati21@gmail.com>
2026-07-18 22:13:47 +0700

    Add files via upload
    hari ke dua

---
+++ cekdata.py
@@ -0,0 +1,10 @@
+a = 4
+b = 2
+c = 4/2
+print(c)
+print(type(c))
+
+d = 9
+e = 3
+print(d / e)
+print(type(d / e))
---
+++ konversidata.py
@@ -0,0 +1,11 @@
+#project_budget=input()
+#print(type(project_budget))
+
+##x = "55"
+#print(type(x))
+#y = int(x)
+#print(type(y))
+
+a = 3
+b = float(a)
+print(b)
